07967d5b35f5543fc069fafe08f0a662edd8b532
khushalibegde <134138053+khushalibegde@users.noreply.github.com>
2025-03-01 19:58:33 +0530

    updated app.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useState } from "react";
-import HomeScreen from "./src/screens/HomeScreen"; // Adjust the path
-import SignUpScreen from "./src/screens/SignUpScreen"; // Adjust the path
+import HomeScreen from "./src/screens/HomeScreen"; 
+import SignUpScreen from "./src/screens/SignUpScreen"; 
 import LoginScreen from "./src/screens/LoginScreen";
+import ContentScreen from "./src/screens/ContentScreen"; 
+import NotesSharing from "./src/screens/NotesSharing";
+import Notes from "./src/screens/Notes";
 
 
 const App = () => {
     const [currentScreen, setCurrentScreen] = useState("Home");
 
-    const navigateTo = (screen) => {
+    const navigateTo = (screen: string) => {
         setCurrentScreen(screen);
     };
     
@@ -17,6 +20,8 @@ const App = () => {
           {currentScreen === "Home" && <HomeScreen navigateTo={navigateTo} />}
           {currentScreen === "SignUp" && <SignUpScreen navigateTo={navigateTo}/>}
           {currentScreen === "Login" && <LoginScreen navigateTo={navigateTo}/>}
+          {currentScreen === "Content" && <ContentScreen navigateTo={navigateTo}/>}
+          {currentScreen === "Notes" && <Notes />}
         </>
     );
     
